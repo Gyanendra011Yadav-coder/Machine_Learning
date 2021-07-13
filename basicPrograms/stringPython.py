40c46a4ff42b9e7b5296d4c2c329bede48eb3f34
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-13 07:12:04 -0700

    METHODS OF STRING IN PYTHON,

---
+++ basicPrograms/stringPython.py
@@ -0,0 +1,9 @@
+name="     manu u know it's not easy to live without you   "
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.count("a"))
+print(name.title().replace(" ",""))
+print(name.lstrip())
+print(name.rstrip())
+print(name.split())
