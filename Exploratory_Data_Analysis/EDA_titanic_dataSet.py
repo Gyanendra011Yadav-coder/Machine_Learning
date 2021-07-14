9fe73cc97b228ea313f79fcb0cf694b7e7178e3a
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-14 09:06:19 -0700

    THIS .describe(), method we are using to print the important details.

--- Exploratory_Data_Analysis/EDA_titanic_dataSet.py
+++ Exploratory_Data_Analysis/EDA_titanic_dataSet.py
@@ -1,8 +1,7 @@
 import pandas as pd
 url = "https://raw.githubusercontent.com/GokuMohandas/MadeWithML/main/datasets/titanic.csv"
 df = pd.read_csv(url, header=0)
-pd.set_option('display.max_columns', None)
- #Show all lines
-pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None) #THIS_WILL_SHOW_COMPLETE_COLUMNS
+pd.set_option('display.max_rows', None)    #THIS_WILL_SHOW_COMPLETE_ROWS
 #pd.set_option('max_colwidth',100)
-print(df.head(3))
+print(df.describe())
